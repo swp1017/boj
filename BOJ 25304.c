fac5aaceb8b78594d1d6808db744009049740bff
swp1017 <swp1017@naver.com>
2022-09-05 12:39:14 +0900

    20220905 commit

---
+++ BOJ 25304.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int temporaryCost, num, i;
+    int realCost = 0;
+
+    scanf("%d", &temporaryCost);
+    scanf("%d", &num);
+
+    for (i=0; i<num; i++)
+    {
+        int a,b;
+        scanf("%d %d", &a, &b);
+        realCost += a*b;
+    }
+
+    if(temporaryCost == realCost)
+    {
+        printf("Yes");
+    }
+
+    else printf("No");
+
+    return 0;
+
+}
