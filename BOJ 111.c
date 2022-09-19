d886d2275c7346a8d97dbaef1c90fa85f2c0e205
swp1017 <swp1017@naver.com>
2022-09-19 17:35:21 +0900

    20220919 commit

--- BOJ 111.c
+++ BOJ 111.c
@@ -2,21 +2,40 @@
 
 int main()
 {
-    int n,i;
-    int num;
-    int box[100];
-    int max;
+    int m;
+    int n;
+    int i;
+    int score[1001];
+    float average = 0;
+    float sum = 0;
 
     scanf("%d", &n);
-
-    for (i=0; i<n; i++)
+    
+    for(i=0; i<n; i++)
     {
-        scanf("%d", &num);
-        box[i] = num;
-        printf("%d\n", box[i]);
+        score[0] = 0;
+        scanf("%f", &score[i+1]);    
     }
 
-    return 0;
+    for(i=0; i<n; i++)
+    {
+        if(score[i]<=score[i+1])
+        {
+            m = score[i+1];
+        }
+    }
+
+    for(i=0; i<n; i++)
+    {
+            score[i+1] = (score[i+1]/m)*100;
+    }
+
+    for(i=0; i<n; i++)
+    {
+        sum += score[i+1];
+    }
+
+    average = sum/(float)n;
+    printf("%f", average);
+
 }
-
-
